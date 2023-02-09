3590ed1adac83d20259b4ccaa0aa68e1e79c0d02
Anton Charapanau <charapanauanton@gmail.com>
2023-02-09 00:51:23 +0000

    20 - NavLink

---
+++ src/components/News/News.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import s from './Dialogs.module.css'
+export const News = (props:any) => {
+    return (
+        <div>
+            News
+        </div>
+    )
+}
--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -4,12 +4,12 @@ import MyPosts from "./MyPosts/MyPosts";
 
 const Profile = () => {
     return (
-        <div className={s.content}>
+        <div>
             <div><img
                 src={"https://img.traveltriangle.com/blog/wp-content/uploads/2018/11/Rivers-of-the-World-Cover.jpg"}/>
             </div>
             <div>
-                ava+discriptiom
+                ava+discription
             </div>
             <MyPosts/>
         </div>
