2d76c27d712fab7b61cb88d9aad65f8d62c556a5
Anton Charapanau <charapanauanton@gmail.com>
2023-02-09 02:10:22 +0000

    21-UI

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -2,8 +2,32 @@ import React from "react";
 import s from './Dialogs.module.css'
 export const Dialogs = (props:any) => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.active}>
+                    Anton
+                </div>
+                <div className={s.dialog}>
+                    Dimych
+                </div>
+                <div className={s.dialog}>
+                    Sveta
+                </div>
+                <div className={s.dialog}>
+                    Sasha
+                </div>
+                <div className={s.dialog}>
+                    Viktor
+                </div>
+                <div className={s.dialog}>
+                    Valera
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.dialog}>Hi</div>
+                <div className={s.dialog}>How is your it-kamasutra?</div>
+                <div className={s.dialog}>Yo</div>
+            </div>
         </div>
     )
 }
