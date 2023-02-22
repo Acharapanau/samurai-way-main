f3161216fb603f06a3bbdba7ca1d7b33b2b20e50
Anton Charapanau <charapanauanton@gmail.com>
2023-02-22 01:56:05 +0000

    24-map

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import s from './Dialogs.module.css'
 import {NavLink} from "react-router-dom";
+import {message} from "antd";
 
 const DialogItem = (props: any) => {
     let path = "/dialogs/" + props.id;
@@ -18,7 +19,7 @@ const Message = (props: any) => {
 }
 
 export const Dialogs = (props: any) => {
-    let dialogsData = [
+    let dialogs = [
         {id: 1, name: 'Anton'},
         {id: 2, name: 'Dimych'},
         {id: 3, name: 'Sveta'},
@@ -26,24 +27,29 @@ export const Dialogs = (props: any) => {
         {id: 5, name: 'Viktor'},
         {id: 6, name: 'Valera'}]
 
-    let messagesData = [
+    let messages = [
         {id: 1, message: 'Hi'},
         {id: 2, message: 'How is your it-kamasutra?'},
         {id: 3, message: 'Yo'},
         {id: 4, message: 'Yo'},
         {id: 5, message: 'Yo'}]
 
+    let dialogsElements = dialogs.map(d => <DialogItem name={d.name} id={d.id}/>);
+
+    let messagesElements = messages.map(m => <Message message={m.message}/>)
 
 
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>
-                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>
+                {dialogsElements}
+                {/*<DialogItem name={dialogs[0].name} id={dialogs[0].id}/>*/}
+                {/*<DialogItem name={dialogs[1].name} id={dialogs[1].id}/>*/}
             </div>
             <div className={s.messages}>
-                <Message message={messagesData[0].message}/>
-                <Message message={messagesData[1].message}/>
+                {messagesElements}
+                {/*<Message message={messagesData[0].message}/>*/}
+                {/*<Message message={messagesData[1].message}/>*/}
             </div>
         </div>
     )
--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -2,22 +2,24 @@ import React from "react";
 import s from './MyPosts.module.css'
 import Post from "./Post/Post";
 
-const MyPosts = (props:any) => {
+const MyPosts = (props: any) => {
 
-    let postData = [
-        {id: 1, post: 'Hi, how are you?',LikesCount:12},
-        {id: 2, post: 'Its my first post',LikesCount: 11},]
+    let posts = [
+        {id: 1, post: 'Hi, how are you?', LikesCount: 12},
+        {id: 2, post: 'Its my first post', LikesCount: 11},]
+
+    let postsElements = posts.map(p => <Post message={p.post} LikesCount={p.LikesCount}/>)
 
     return (<div className={s.postsBlock}>
-           <h3> My posts</h3>
+            <h3> My posts</h3>
             <div>
                 <div><textarea></textarea></div>
-               <div><button>Add post</button></div>
+                <div>
+                    <button>Add post</button>
+                </div>
             </div>
             <div className={s.posts}>
-               <Post message={postData[0].post} LikesCount={postData[0].LikesCount}/>
-                <Post message={postData[1].post} LikesCount={postData[1].LikesCount}/>
-
+                {postsElements}
             </div>
         </div>
     );
